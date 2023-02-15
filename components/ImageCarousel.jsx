459b20cf1c5c9b4dba03b9599522574e4f185bb2
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-02-15 10:31:43 +0530

    image carousel

---
+++ components/ImageCarousel.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
+
+function ImageCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel
+      activeIndex={index}
+      onSelect={handleSelect}
+      interval={4000}
+      style={{ height: "100vh" }}
+    >
+      <Carousel.Item>
+        <img
+          className="d-block w-100 h-100 "
+          src="homepage/banner-1.png"
+          alt="Image 1"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="homepage/banner-2.png"
+          alt="Image 2"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="homepage/banner-3.png"
+          alt="Image 3"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ImageCarousel;
