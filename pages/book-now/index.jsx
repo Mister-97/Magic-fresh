35267c356102e6e47103736eb80bdd9e1a917f4b
captshubh <126143767+captshubh@users.noreply.github.com>
2023-02-23 12:35:15 +0530

    Update index.jsx

--- pages/book-now/index.jsx
+++ pages/book-now/index.jsx
@@ -3,53 +3,6 @@ import styled from "styled-components";
 import Point from "../../components/Legal/Point";
 import Select from "react-select";
 import { Btn } from "../contact";
-
-const BookNow = () => {
-  const optionsService = [
-    {
-      value: "residential",
-      label: "Residential Cleaning",
-    },
-    {
-      value: "airbnb",
-      label: "Airbnb",
-    },
-    {
-      value: "move",
-      label: "Move In/Out Service",
-    },
-    {
-      value: "carpet",
-      label: "Carpet Cleaning",
-    },
-  ];
-  const optionsType = [
-    {
-      value: "regular",
-      label: "Regular Cleaning",
-    },
-    {
-      value: "deepClean",
-      label: "Deep Cleaning",
-    },
-    {
-      value: "Move",
-      label: "Move In/Out Service",
-    },
-  ];
-
-  const optionsBedrooms = [
-    { value: "Studio", label: "Studio" },
-    { value: "1br/1ba", label: "1br/1ba" },
-    { value: "2br/1ba", label: "2br/1ba" },
-    { value: "2br/2ba", label: "2br/2ba" },
-    { value: "3br/1ba", label: "3br/1ba" },
-    { value: "3br/2ba", label: "3br/2ba" },
-    { value: "3br/2ba", label: "3br/3ba" },
-    { value: "4br+", label: "4br+" },
-  ];
-
-  return (
     <Container>
       <br />
       <Point>
@@ -80,8 +33,6 @@ const BookNow = () => {
       <br />
       <Img src="/booking/buildings.png" />
     </Container>
-  );
-};
 
 export default BookNow;
 
