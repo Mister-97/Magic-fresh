99af46e5233a98655766276487f22de7318e5559
Joshua Porter <89230912+Mister-97@users.noreply.github.com>
2023-02-26 10:35:17 +0530

    Update index.jsx

--- pages/book-now/index.jsx
+++ pages/book-now/index.jsx
@@ -48,22 +48,13 @@ const BookNow = () => {
     { value: "3br/2ba", label: "3br/3ba" },
     { value: "4br+", label: "4br+" },
   ];
-  
-  class App extends React.Component {
-   render(){
-      return (
-test
-      );
-   }
-}
-export default App;
 
   return (
     <Container>
       <br />
       <Point>
         <h2>Request a Quote</h2>
-
+        <iframe name="cYjZUcwiYjZUcw" id="cYjZUcwiYjZUcw" src="//app.shform.com/app/embed?id=YjZUcw&amp;sid=0&amp;p=1&amp;t=1&amp;reset=1&amp;js=1&amp;rec=1&amp;parentUrl=https%3A%2F%2Fapp.shform.com%2Fapp%2Fform%3Fid%3DYjZUcw&amp;title=Request%20a%20Quote%20magicfresh.cleaning&amp;url=https%3A%2F%2Fapp.shform.com%2Fapp%2Fform%3Fid%3DYjZUcw&amp;referrer=https%3A%2F%2Fapp.shform.com%2Fform%2Fshare%3Fid%3D292" scrolling="no" frameborder="0" style="width: 100%; height: 570px;"></iframe>
       </Point>
       <br />
       <Img src="/booking/buildings.png" />
